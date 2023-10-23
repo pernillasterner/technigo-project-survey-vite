51da645712bdaa77b329f8b5d7b61a0b49b27412
psterner <psterner@hotmail.com>
2023-10-23 13:41:56 +0200

    Create Form component and add styling to form title

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { FormBody } from "./components/Form/Form";
+
+/**
+ * Need to send this data to form
+ * - id
+ * - question info
+ * - question body
+ *
+ */
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <div className="form">
+      <FormBody />
+    </div>
+  );
 };
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,9 @@
+export const FormBody = () => {
+  return (
+    <div className="form-body">
+      <div class="form-title">
+        <h1>🧊 Welcome to the IceBreaker Quiz 🧊</h1>
+      </div>
+    </div>
+  );
+};
