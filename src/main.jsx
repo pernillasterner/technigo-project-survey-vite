aad597f56882058138a8a48e4e7c42412665b54f
psterner <psterner@hotmail.com>
2023-10-30 11:06:52 +0100

    Reverse changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-import "../dist/assets/css/index.css";
+import "../dist/assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
