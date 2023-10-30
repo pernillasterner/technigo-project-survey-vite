8aaf40a07817c51c6e7e927959a7d2cc9fe37563
psterner <psterner@hotmail.com>
2023-10-30 11:06:04 +0100

    Test change path to css folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-import "../dist/assets/styles/index.css";
+import "../dist/assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
