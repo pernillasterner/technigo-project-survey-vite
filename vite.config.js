47a7fd9a7143e72d9c8e1dfce1446d403bc57e35
psterner <psterner@hotmail.com>
2023-10-30 11:12:00 +0100

    Output CSS with hashed filenames

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Output CSS with hashed filenames
+      output: {
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].css", // Adjust the naming pattern as needed
+      },
+    },
+  },
+});
