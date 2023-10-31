ef3ceb8847ff5169caf60b6778edc64f3ff3f565
psterner <psterner@hotmail.com>
2023-10-31 08:51:10 +0100

    Reset changes

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].css",
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Provide the path to your main SCSS file
-        additionalData: `@import "@/src/assets/styles/index.scss";`,
-      },
-    },
-  },
 });
