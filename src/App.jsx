be76e32f9f22af43ca1b7e9f66b6e98f4c6ee779
Pernilla Sterner <psterner@hotmail.com>
2024-03-08 22:06:45 +0100

    Update all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import { MultiStepForm } from "./components/Form/MultiStepForm";
-import IceBreakerData from "./icebreaker-questions.json";
-
-/**
- * Need to send this data to form
- * - id
- * - question info
- * - question body
- *
- */
-
 export const App = () => {
-  const questions = IceBreakerData.questions;
-
-  return <div className="form">{<MultiStepForm questions={questions} />}</div>;
+  return <div>Find me in src/app.jsx!</div>;
 };
