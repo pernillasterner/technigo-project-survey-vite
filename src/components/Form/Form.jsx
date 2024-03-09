26c7669adeb84a2a24d1b1a1e5188d6f95e4c694
Pernilla Sterner <psterner@hotmail.com>
2024-03-09 21:32:35 +0100

    Handle input state and lift state from input component to form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,23 +3,40 @@ import { useState } from "react";
 import { questions } from "../../data/questions.json";
 import { Question } from "./FormContent/Question/Question";
 import { Button } from "./FormContent/Button/Button";
-import { InputType } from "./FormContent/InputTypes/InputType";
+import { InputType } from "./FormContent/InputType/InputType";
 
 export const Form = () => {
   const [questionId, setQuestionId] = useState(1);
+  const [answers, setAnswers] = useState({});
 
-  const renderForm = questions.map(({ id, question, type, options }) =>
+  const onInputChange = (id, value) => {
+    console.log(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(answers);
+  };
+
+  // Pass form props and render the form
+  const renderForm = questions.map(({ id, question, type, options, name }) =>
     questionId === id ? (
-      <div key={id} className="form__container">
+      <form key={id} className="form__container" onSubmit={handleSubmit}>
         <>
           {/* question container */}
           <Question id={id} question={question} />
           {/* input type */}
-          <InputType type={type} options={options} />
+          <InputType
+            id={id}
+            type={type}
+            options={options}
+            name={name}
+            onInputChange={onInputChange}
+          />
           {/* button */}
-          {/* <Button id={id} /> */}
+          <Button />
         </>
-      </div>
+      </form>
     ) : null
   );
 
--- src/components/Form/FormContent/InputTypes/InputType.jsx
+++
@@ -1,6 +0,0 @@
-import "./InputType.scss";
-
-export const InputType = ({ type, options }) => {
-  console.log(type, options);
-  return <div>InputType</div>;
-};
