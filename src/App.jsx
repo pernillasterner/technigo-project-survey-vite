5ce1007ce6c84423cafa6d9e015ff1d0c45bc6b9
Pernilla Sterner <psterner@hotmail.com>
2024-03-09 17:40:54 +0100

    Basic folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Form } from "./components/Form/Form";
 import { Header } from "./components/Header/Header";
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Form />
+    </>
   );
 };
