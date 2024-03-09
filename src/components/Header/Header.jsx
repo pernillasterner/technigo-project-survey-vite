b9adfa6f6cec0b64fd21c547f976f1ddbf44d02c
Pernilla Sterner <psterner@hotmail.com>
2024-03-09 18:33:03 +0100

    Add title and subtitle. Improve header scaling (#2)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,12 @@ import "./Header.scss";
 
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className="header__title">
+        <h1>The Science of Happiness</h1>
+      </div>
+      {/* TODO:make subitle dynamic */}
+      <div className="header__subtitle">Everyday Joy Boosters survey</div>
+    </header>
   );
 };
