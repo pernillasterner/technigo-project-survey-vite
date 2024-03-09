257858c8229baeeffbd05aff5e8f6ac47a25bcd7
Pernilla Sterner <psterner@hotmail.com>
2024-03-09 18:01:18 +0100

    Feature folder setup (#1)
    * Import ViteSassPlugin and test color vaiable
    
    * Basic folder structure
    
    * Update with link to stage env and description of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { Form } from "./components/Form/Form";
+import { Header } from "./components/Header/Header";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  );
 };
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,5 @@
+import "./Form.scss";
+
+export const Form = () => {
+  return <div>Form</div>;
+};
