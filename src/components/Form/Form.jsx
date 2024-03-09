f8ab2ed4c91a3b603e7a04271180fc3e644675be
Pernilla Sterner <psterner@hotmail.com>
2024-03-09 19:04:36 +0100

    Add first question

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,19 @@
 import "./Form.scss";
+import { useState } from "react";
+import { questions } from "../../data/questions.json";
 
 export const Form = () => {
-  return <div>Form</div>;
+  const [questionId, setQuestionId] = useState(1);
+
+  const renderForm = questions.map(({ id, question, type, options }) => (
+    <div key={id} className="question__container">
+      {questionId === id && (
+        <>
+          <p>{question}</p>
+        </>
+      )}
+    </div>
+  ));
+
+  return <section className="form__container">{renderForm}</section>;
 };
