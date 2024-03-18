cc2d7f06c702a5863eec97f36e67cfc72a22d931
Pernilla Sterner <psterner@hotmail.com>
2024-03-18 15:31:21 +0100

    Add initial page to start the survey (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Form } from "./components/Form/Form";
 import { Header } from "./components/Header/Header";
+import { useState } from "react";
 
 export const App = () => {
+  const [step, setStep] = useState(0);
+
+  const handleButtonClick = () => {
+    // Update step when button gets clicked on
+    setStep(step + 1);
+  };
+
   return (
     <>
-      <Header />
-      <Form />
+      {step === 0 ? (
+        <Header step={step} onClick={handleButtonClick} />
+      ) : (
+        <Form step={step} />
+      )}
     </>
   );
 };
--- src/components/UIElements/Button/Button.jsx
+++ src/components/UIElements/Button/Button.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Button.scss";
 
 // Need to add an eventlistener to button. Go to next question
-export const Button = () => {
+export const Button = ({ step, onClick }) => {
   // Set useState on the classname of the button
   const [buttonState, setButtonState] = useState("default");
-  // Get id as prop and pass
+  const [buttonText, setButtonText] = useState("");
 
-  return <button className={buttonState}>Submit your answer</button>;
+  useEffect(() => {
+    switch (step) {
+      case 0:
+        setButtonText("Let's go!");
+        break;
+      case 1:
+        setButtonText("Next question");
+        break;
+      default:
+        // Set default button text here
+        setButtonText("Continue");
+        break;
+    }
+  }, [step]);
+
+  return (
+    <button className={buttonState} onClick={onClick}>
+      {buttonText}
+    </button>
+  );
 };
